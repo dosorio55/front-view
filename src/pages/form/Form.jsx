1128504735d979f919eae053c69b8fdc74659b54
dosorio55 <dosorio55@gmail.com>
2022-06-08 09:08:01 +0200

    fetch post for login working

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
 
 const Form = () => {
+
+  const [formsState, setFormsState] = useState(INITIAL_STATE)
+
+  const handleInput = (ev) => {
+    const { name, value } = ev.target;
+    setFormsState({ ...formsState, [name]: value });
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault(event)
+    fetch('http://localhost:4000/user', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formsState)
+    }).then(() => {
+      console.log(`the user ${formsState}`)
+    })
+
+    console.log(formsState)
+
+  }
+
   return (
-    <div>Form</div>
+    <form onSubmit={submitForm}>
+      <label>
+        <p>email</p>
+        <input type="text" name='email' value={formsState.email} onChange={handleInput} />
+      </label>
+      <label>
+        <p>password</p>
+        <input type="text" name='password' value={formsState.password} onChange={handleInput} />
+      </label>
+      <div>
+        <button type="submit">Guardar Perfil</button>
+      </div>
+
+    </form>
   )
 }
 
